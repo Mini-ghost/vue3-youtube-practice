47169978022ef4765f6c4b3e359da29bf5096317
Alex Liu <dsa1314@gmail.com>
2020-09-25 13:54:50 +0800

    chore: 調整 publicPath 設定
    - publicPath 在 production 時設定為相對路徑

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/youtube-api-with-vue3/'
-    : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
 }
